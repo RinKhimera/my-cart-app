ce1e6048141c6a02c58802123360e13779e4f3b4
Samuel Pokam <dixiades@gmail.com>
2022-12-01 00:48:57 +0100

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,32 +3,30 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <span className={this.getBadgesClasses()}>{this.formatExe()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>Nourris Doggy Drogo</p>;
+    } else {
+      return (
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.tags.length === 0 && "Essaie de le nourrir"}
+        {this.renderTags()}
       </React.Fragment>
     );
-  }
-
-  getBadgesClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatExe() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
