260b76e6847153c4d68b853b80f34545d0c1610f
Samuel Pokam <dixiades@gmail.com>
2022-12-04 20:25:40 +0100

    Display of total carts added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.js";
-import Counters from "./components/counters";
 
 ReactDOM.render(<App />, document.getElementById("root"));
